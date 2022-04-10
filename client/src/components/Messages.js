accf7d829c6a5b77cef8e4a8ac6b2789b3a034af
Kevin Wang <kevqwang@gmail.com>
2022-04-10 00:48:24 -0400

    all memssages

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import Message from './Message'
 
-function Messages() {
+function Messages({ messages, currentMember, handleMessageDelete }) {
+  const messageEndRef = useRef(null)
+
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+  }, [messages.length])
+
   return (
-    <div>Messages</div>
+    <div className='messages'>
+      {
+        messages.map(message => <Message key={message.id} message={message} currentMember={currentMember} handleMessageDelete={handleMessageDelete}/>)
+      }
+      <div ref={messageEndRef}/>
+    </div>
   )
 }
 
