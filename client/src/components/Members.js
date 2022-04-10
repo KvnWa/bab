03f2e5971186b923d7dd9b58a39ef63becac59dd
Kevin Wang <kevqwang@gmail.com>
2022-04-10 00:45:36 -0400

    all members

--- client/src/components/Members.js
+++ client/src/components/Members.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Member from './Member'
 
-function Members() {
+function Members({ members }) {
   return (
-    <div>Members</div>
+    <div className='members'>
+      <h3>Members:</h3>
+      <div>
+        {
+          members.map(member => <Member key={member.id} member={member}/>)
+        }
+      </div>
+    </div>
   )
 }
 
