86fb1a155b175f2cdd7d68aa76fd3ef3a7a59df7
Kevin Wang <kevqwang@gmail.com>
2022-04-10 00:23:25 -0400

    signup

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,8 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { signUpFormDefaultValues, profileImages } from './helpers/formhelpers.js'
 
-function SignUpForm() {
+function SignUpForm({ handleUser }) {
+  const [formData, setFormData] = useState(signUpFormDefaultValues)
+  const [errors, setErrors] = useState([])
+
+  const navigate = useNavigate()
+
+  function handleFormChange(e) {
+    const { name, value } = e.target
+    setFormData(formData => ({ ...formData, [name]: value }))
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(r => {
+      if (r.ok) {
+        r.json().then(user => {
+          setFormData(signUpFormDefaultValues)
+          navigate('/')
+          handleUser(user)
+        })
+      } else {
+        r.json().then(e => {
+          setErrors(() => e.errors)
+        })
+      }
+    })
+  }
+
   return (
-    <div>SignUpForm</div>
+    <div className='sign-up-form'>
+      <form onSubmit={handleFormSubmit}>
+        <div className='sign-up-form-containers'>
+          <div className='sign-up-form-container-1'>
+            <br />
+            <label>Username:
+              <br/>
+              <input 
+                type='text'
+                name='username'
+                value={formData.username}
+                onChange={handleFormChange}
+              />
+            </label>
+            <br/>
+            <label>Password:
+              <br/>
+              <input 
+                type='password'
+                autoComplete='off'
+                name='password'
+                value={formData.password}
+                onChange={handleFormChange}
+              />
+            </label>
+            <br/>
+            <label>Password Confirmation:
+              <br/>
+              <input 
+                type='password'
+                autoComplete='off'
+                name='password_confirmation'
+                value={formData.password_confirmation}
+                onChange={handleFormChange}
+              />
+            </label>
+          </div>
+          <div className='sign-up-form-container-2'>
+            <br />
+            <label>Choose Profile Image:
+              <br />
+              <select name='image_url' value={formData.image_url} onChange={handleFormChange}>
+                <option value=''>Default</option>
+                {
+                  profileImages.map(({ name, image_url }) =>
+                    <option key={name} value={image_url}>{name}</option>
+                  )
+                }
+              </select>
+              <br />
+              <img className='img-2' src={formData.image_url ||  logo} alt='default profile face'/>
+            </label>
+          </div>
+        </div>
+        <input className='bttn-1 submit-button' type='submit' value='Register'/>
+      </form>
+      {
+        errors.map(error => 
+          <p key={error} className='form-error'>{error}</p>
+        )
+      }
+      <p><Link to='/'>Sign in</Link> if account<br/>does exist</p>
+    </div>
   )
 }
 
