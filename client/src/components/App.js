c1167893446974519d0cce044f95cdedd987e32d
Kevin Wang <kevqwang@gmail.com>
2022-04-10 00:50:53 -0400

    app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
-// client/src/components/App.js
 import { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from './Login'
 import Header from './Header'
-
+import Chatroom from "./Chatroom";
+import ChatroomSearch from "./ChatroomSearch";
+import MembershipNav from "./MembershipNav";
+import Welcome from './Welcome'
+import UserProfile from "./UserProfile";
 
 function App() {
   const [user, setUser] = useState(null)
@@ -56,7 +59,13 @@ function App() {
     return (
       <Routes>
         <Route path="/" element={<Header handleUser={handleUser} user={user}/>}>
-        
+          <Route path="chatrooms" element={<MembershipNav memberships={user.memberships}/>}>
+            <Route path=":chatroomId/*" element={<Chatroom user={user} handleChatroomMembershipWithdrawal={handleChatroomMembershipWithdrawal}/>}/>
+          </Route>
+          <Route path="chatrooms/search" element={<ChatroomSearch handleChatroomMembership={handleChatroomMembership} />}/>
+          <Route index element={<Welcome user={user}/>} />
+          <Route path="profile" element={<UserProfile user={user} handleUser={handleUser} />}/>
+        </Route>
         <Route path="*" element={<Navigate to="/"/> }/>
       </Routes>
     )
