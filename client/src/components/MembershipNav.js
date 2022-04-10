45b5494b7c84fda4ec574ba2e7ebc3296a72f489
Kevin Wang <kevqwang@gmail.com>
2022-04-10 00:46:55 -0400

    nav

--- client/src/components/MembershipNav.js
+++ client/src/components/MembershipNav.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+import logo from '../images/babblelogo.png'
 
-function MembershipNav() {
+function MembershipNav({ memberships }) {
+  const activeStyle = { backgroundColor: '#121212' }
   return (
-    <div>MembershipNav</div>
+    <div className='membership-nav'>
+      <nav>
+        <h3>Spoke</h3>
+        {
+          memberships.map(({id, name, image_url}) => {
+            return (
+              <NavLink 
+                className='membership-nav-button' 
+                key={id} 
+                to={`/chatrooms/${id}`}
+                style={({ isActive }) =>
+                isActive ? activeStyle : undefined
+                }
+                >
+                  <img src={image_url || logo} alt={image_url === '' ? '' : ''} />
+                  <h4>{name}</h4>
+              </NavLink>
+            )
+          })
+        }
+      </nav>
+      <Outlet />
+    </div>
   )
 }
 
