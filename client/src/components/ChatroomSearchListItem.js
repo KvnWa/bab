94beac01d0383e38d1bc1ee0383f7edfa18ba126
Kevin Wang <kevqwang@gmail.com>
2022-04-10 00:39:35 -0400

    list items

--- client/src/components/ChatroomSearchListItem.js
+++ client/src/components/ChatroomSearchListItem.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import logo from '../images/tophatmonocle-hat-transparent.png'
 
-function ChatroomSearchListItem() {
+
+function ChatroomSearchListItem({ chatroom, selectedChatroom, handleSelectedChatroom }) {
+  const { id, name, image_url, bio } = chatroom
+
+  function handleChatroomClick() {
+    handleSelectedChatroom(id)
+  }
+
   return (
-    <div>ChatroomSearchListItem</div>
+    <div className={selectedChatroom === id ? 'search-item selected' : 'search-item'} onClick={handleChatroomClick}>
+      <img src={image_url || logo} alt={image_url === '' ? 'moustache man' : 'chatroom image'} />
+      <h3>{name}</h3>
+      <p>{bio}</p>
+    </div>
   )
 }
 
