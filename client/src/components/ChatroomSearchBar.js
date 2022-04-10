e052a4a8bdb0aa385da692f1d4448f096797173d
Kevin Wang <kevqwang@gmail.com>
2022-04-10 00:37:07 -0400

    search bar

--- client/src/components/ChatroomSearchBar.js
+++ client/src/components/ChatroomSearchBar.js
@@ -1,8 +1,34 @@
 import React from 'react'
 
-function ChatroomSearchBar() {
+function ChatroomSearchBar({ search, handleSearch, searchBy, handleSearchBy }) { 
+  function handleSearchChange(e) {
+    handleSearch(e.target.value)
+  }
+
+  function handleRadioChange(e) {
+    handleSearchBy(e.target.value)
+  }
+
   return (
-    <div>ChatroomSearchBar</div>
+    <div className='chatroom-search-bar'>
+      <input 
+        type='text'
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <fieldset>
+        <label>
+          <input 
+            type='radio' 
+            name='search' 
+            value='name'
+            checked={searchBy === 'name'} 
+            onChange={handleRadioChange}
+          />
+          By Name
+        </label>
+      </fieldset>
+    </div>
   )
 }
 
