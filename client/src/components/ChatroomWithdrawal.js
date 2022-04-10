7ab848bef4441de81c7b421bbfebc23878fd81fb
Kevin Wang <kevqwang@gmail.com>
2022-04-10 00:40:38 -0400

    channel leave

--- client/src/components/ChatroomWithdrawal.js
+++ client/src/components/ChatroomWithdrawal.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-function ChatroomWithdrawal() {
+function ChatroomWithdrawal({ currentMember, handleChatroomMembershipWithdrawal }) {
+  const navigate = useNavigate()
+
+  function handleChatroomWithdrawal() {
+    fetch(`/api/chatrooms/${currentMember.chatroom_id}/chatroom_memberships/${currentMember.id}`, {
+      method: 'DELETE'
+    })
+    .then(r => {
+      if (r.ok) {
+        r.json().then(chatroom => {
+          handleChatroomMembershipWithdrawal(chatroom)
+          navigate('/chatrooms')
+        })
+      }
+    })
+  }
+
   return (
-    <div>ChatroomWithdrawal</div>
+    <div className='chatroom-withdrawal'>
+      <button
+        onClick={handleChatroomWithdrawal}
+        className='bttn-1'
+      >Leave<br/> Channel</button>
+    </div>
   )
 }
 
